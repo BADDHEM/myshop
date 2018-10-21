bcb1980c8eadea19cd36729bd8b32e27c775589b
vamsi <tovamsich@gmail.com>
2018-10-21 13:10:51 +0530

    commiting

---
+++ Myshop/src/app/login/login.component.ts
@@ -0,0 +1,14 @@
+import { Component} from "@angular/core";
+
+
+
+
+@Component({
+    moduleId: module.id,
+    templateUrl: "./login.component.html",
+    styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+   
+}
+
